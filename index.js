5644f7ea8c704a68a7bbe2a2ace26a1ea7e90f0e
Pekka Laaksonen <pekka.laaksonen@iki.fi>
2017-12-11 22:08:27 +0200

    modified:   index.js 	new file:   public/index.html

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express")
-
 const app = express()
-
 const port = 3000
+const path = require("path")
+
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get("/", (req, res) => {
+    res.send("Hello world.")
+})
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`)
