4fe96799d68938c9275fc86a03b18a2020695215
Pekka Laaksonen <pekka.laaksonen@iki.fi>
2020-03-04 16:29:48 +0200

    eslint configs, prettier etc

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const path = require('path')
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
 
-app.use(express.static(path.join(__dirname, 'build')))
+app.use(express.static(path.join(__dirname, "build")));
 //app.use('/metaballs', express.static(path.join(__dirname, 'public/code/metaballs')))
 
 app.use((req, res) => {
   if (res.status(404)) {
-    res.send('404')
+    res.send("404");
   }
-})
+});
 
 app.listen(port, () => {
-  console.log(`Server started on port ${port}`)
-})
+  console.log(`Server started on port ${port}`);
+});
