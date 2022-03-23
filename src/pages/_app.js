4b2a3b9a2cfb179b17c3a6cb88a9c548bd8c5003
Pekka Laaksonen <pekka.laaksonen@iki.fi>
2022-03-23 11:41:01 +0200

    moved stylesheets to document

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,14 @@
 import "../styles/index.css";
 import "../styles/App.css";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Pekka Laaksonen</title>
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
